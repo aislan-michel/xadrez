564765cfba81c9df2ac27f6ee3b3f7f27f618c3a
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2020-02-16 12:06:54 -0300

    updated method PrintTray

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -7,11 +7,15 @@ namespace ChessConsole {
         static void Main(string[] args) {
             //padrao de camadas - tabuleiro, jogo de xadrez, aplicação
             try {
-                ChessPosition pos = new ChessPosition('c', 7);
+                Board board = new Board(8, 8);
 
-                Console.WriteLine(pos);
+                board.InsertPiece(new Tower(board, Color.Preta), new Position(0, 0));
+                board.InsertPiece(new Tower(board, Color.Preta), new Position(1, 3));
+                board.InsertPiece(new King(board, Color.Preta), new Position(0, 2));
 
-                Console.WriteLine(pos.ToPosition());
+                board.InsertPiece(new Tower(board, Color.Branca), new Position(3, 5));
+
+                Screen.printTray(board);
 
             } catch (BoardException e) {
                 Console.WriteLine(e.Message);
--- XadrezConsole/Screen.cs
+++ XadrezConsole/Screen.cs
@@ -6,17 +6,35 @@ namespace ChessConsole {
         public static void printTray(Board board) {
 
             for (int i = 0; i < board.Rows; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++) {
                     if (board.Piece(i, j) == null) {
                         Console.Write("- ");
 
                     } else {
-                        Console.Write(board.Piece(i, j) + " ");
+                        PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
 
                     }
 
                 }
                 Console.WriteLine();
+
+            }
+            Console.WriteLine("  a b c d e f g h");
+
+        }
+
+        public static void PrintPiece(Piece piece) {
+            if (piece.Color == Color.Branca) {
+                Console.Write(piece);
+
+            } else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+
+                Console.ForegroundColor = aux;
 
             }
 
