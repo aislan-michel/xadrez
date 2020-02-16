913d05dcb28e539283a62fd6fa7ce9745e023b77
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2020-02-15 21:52:18 -0300

    position class added

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using Board;
 
 namespace XadrezConsole {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            //padrao de camadas - tabuleiro, jogo de xadrez, aplicação
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine(P);
         }
     }
 }
